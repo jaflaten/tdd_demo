f3c191ba31487f81eaa534d36369b57e1bd310a2
jaflaten <ja.flaten91@gmail.com>
2022-02-07 14:27:20 +0100

    added REST endpoint to calculate BMI

--- src/main/java/com/example/tdd_demo/bmi/BmiController.java
+++ src/main/java/com/example/tdd_demo/bmi/BmiController.java
@@ -1,11 +1,26 @@
 package com.example.tdd_demo.bmi;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class BmiController {
 
     private final BmiService service;
 
-    //TODO caluclate BMI .
+    @GetMapping("/bmi/{weight}/{height}")
+    public ResponseEntity<?> calculateBmi(@PathVariable double weight, @PathVariable double height) {
+        if (weight == 0 || height == 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        service.calculateBmi(weight, height);
+        return ResponseEntity.ok(service.calculateBmi(weight, height));
+    }
 }
